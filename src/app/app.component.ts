80c177d8fc5d56c2c9df6cf622bcde1c48c5f9cd
Luan Trajano <luan.trajano@biz.com.br>
2025-04-11 19:03:13 -0300

    feat: created design system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { BtnModule } from '../../projects/btn/src/public-api';
+
+import { BtnModule } from '@design_front/btn';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [BtnModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'organize';
